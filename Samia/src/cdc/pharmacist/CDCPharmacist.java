8cf62e7ba70cddf5ed3842ad2bc7463247b0e4c8
samia-saba-09 <samiasaba09@gmail.com>
2024-04-16 03:48:54 +0600

    FINISH MENU

--- Samia/src/cdc/pharmacist/CDCPharmacist.java
+++ Samia/src/cdc/pharmacist/CDCPharmacist.java
@@ -19,9 +19,7 @@ public class CDCPharmacist extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("PLogin.fxml"));
-        
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
--- Samia/src/cdc/pharmacist/MenuController.java
+++ Samia/src/cdc/pharmacist/MenuController.java
@@ -88,8 +88,18 @@ public class MenuController implements Initializable {
         if (event.getSource() == menuSupplier) {
         } else if (event.getSource() == menuItemCreateSupplier) {
             try {
-                // Load CreateSupplier.fxml
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateSupplier.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (event.getSource() == menuItemListSupplier) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Supplier.fxml"));
                 Parent root = loader.load();
 
                 Stage stage = new Stage();
@@ -99,20 +109,107 @@ public class MenuController implements Initializable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } else if (event.getSource() == menuItemListSupplier) {
         } else if (event.getSource() == menuMedicine) {
         } else if (event.getSource() == menuItemCreateMedicine) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateMedicine.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuItemListMedicine) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Medicine.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuInventory) {
         } else if (event.getSource() == menuItemCreateInventory) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateInventory.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuItemListInventory) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Inventory.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuInvoice) {
         } else if (event.getSource() == menuItemCreateInvoice) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateInvoice.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuItemListInvoice) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Invoice.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuSales) {
         } else if (event.getSource() == menuItemListSales) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Sales.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuTrackReceipt) {
         } else if (event.getSource() == menuItemListTrackReceipt) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("TrackInvoice.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Create Supplier");
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (event.getSource() == menuLogout) {
         }
     }
